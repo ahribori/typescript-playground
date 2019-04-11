78ec903ffa1bdc759c0445dd51950d5a20391789
ahribori <ahribori@gmail.com>
2019-04-12 01:00:49 +0900

    Promise 기반 transaction 예제 따라하기, 근데 잘 안된다 슈밤

--- src/mongoose/examples/transaction.ts
+++ src/mongoose/examples/transaction.ts
@@ -1,15 +1,39 @@
 import * as mongoose from 'mongoose';
-import User from '../schemas/UserSchema';
+
+const Customer = mongoose.model(
+  'Customer',
+  new mongoose.Schema({ name: String }),
+);
 
 export const transactionalTask = async () => {
-  const session = await mongoose.startSession();
-  session.startTransaction();
-  try {
-    await User.create({ id: 1, name: 'Daniel', age: 30 }, { session });
-    await session.commitTransaction();
-  } catch (e) {
-    console.log(e);
-    await session.abortTransaction();
-  }
-  session.endSession();
+  let session: any = null;
+  mongoose
+    .startSession()
+    .then(_session => {
+      session = _session;
+      session.startTransaction();
+      return Customer.create({ name: 'daniel' }, { session });
+    })
+    .then(() => Customer.findOne({ name: 'daniel' }))
+    .then(doc => {
+      console.log(doc);
+    })
+    .then(() => Customer.findOne({ name: 'daniel' }).session(session))
+    .then(doc => console.log(doc))
+    .then(() => session.commitTransaction())
+    .then(() => Customer.findOne({ name: 'daniel' }))
+    .then(doc => console.log(doc))
+    .catch(e => {
+      console.log(e)
+    })
+  // const session = await mongoose.startSession();
+  // session.startTransaction();
+  // try {
+  //   await User.create({ id: 1, name: 'Daniel', age: 30 }, { session });
+  //   await session.commitTransaction();
+  // } catch (e) {
+  //   console.log(e);
+  //   await session.abortTransaction();
+  // }
+  // session.endSession();
 };
